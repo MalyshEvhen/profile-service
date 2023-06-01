6b9cb5f44a479e9d1ca0252283693add2d7c33ea
Evhen <malysh.evgeniy@gmail.com>
2023-06-01 17:10:45 +0300

    add builder

--- src/main/java/ua/malysh/domain/Diet.java
+++ src/main/java/ua/malysh/domain/Diet.java
@@ -5,11 +5,13 @@ import jakarta.persistence.Embeddable;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Embeddable
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Diet {
@@ -32,4 +34,15 @@ public class Diet {
 
     @Column(name = "diet_protein")
     private Double proteinNorm;
+
+    public static Diet getDefault() {
+        return Diet.builder()
+            .type(DietType.WO_DIET)
+            .calorieNorm(2000D)
+            .carbohydrates(0D)
+            .waterNorm(2000D)
+            .fatNorm(0D)
+            .proteinNorm(0D)
+            .build();
+    }
 }
