af6734c515d5d3844b6196f8139f9196d1ff4563
Evhen <malysh.evgeniy@gmail.com>
2023-06-01 15:41:13 +0300

    add coefs to Diet types

--- src/main/java/ua/malysh/domain/DietType.java
+++ src/main/java/ua/malysh/domain/DietType.java
@@ -1,7 +1,17 @@
 package ua.malysh.domain;
 
 public enum DietType {
-    LOOSE_WEIGHT,
-    GAINING_WEIGHT,
-    MAINTENANCE_WEIGHT
+    LOOSE_WEIGHT(0.8),
+    GAINING_WEIGHT(1.2),
+    MAINTENANCE_WEIGHT(1.0);
+
+    private final double typeCoefficient;
+
+    private DietType(double coefficient) {
+        this.typeCoefficient = coefficient;
+    }
+
+    public double getTypeCoefficient() {
+        return typeCoefficient;
+    }
 }
